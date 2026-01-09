1f8be8ddeff18d4e80aac7a6deb4777aa2e277b8
ivancvek <ivancvej@gmail.com>
2026-01-09 10:02:16 +0100

    Delete types.ts

--- types.ts
+++
@@ -1,4 +0,0 @@
-
-export type CategoryType = 'hrana' | 'zabava';
-export interface Option { id: string; label: string; icon: string; }
-export interface DecisionResult { category: CategoryType; option: Option; aiSuggestion?: string; }
